7906e180043192a2f93669f07b04fe93b41a721d
paulstrong7 <paulstrong7@gmail.com>
2025-11-02 19:12:15 -0700

    reverting config.py, adding 404.html

--- WEBSITE/config.py
+++ WEBSITE/config.py
@@ -4,5 +4,5 @@ DB_PASSWORD = "IFT401_25"
 DB_HOST = "my-rds-instance.cxq8kcsuioc3.us-east-2.rds.amazonaws.com"
 DB_PORT = "3306"
 DB_NAME = "project_stocks"
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:IFT401_25@my-rds-instance.cxq8kcsuioc3.us-east-2.rds.amazonaws.com/project_stocks'
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
